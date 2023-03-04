777b064e0bf14573ffc7f4a0cf5b1831f7686106
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2023-03-04 17:26:04 +0100

    import movie list

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+//Include the backend so that it can be reached writing fetch(‘/api/rest-route’) 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cinema-rest.nodehill.se',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
